511a37f3cce37c5b6caee3a95e7317b524585caf
limekh <167549380+limekh@users.noreply.github.com>
2025-04-04 13:32:47 +0400

    Update task1/consts.py

--- lab_1/task1/consts.py
+++ lab_1/task1/consts.py
@@ -1,1 +1,8 @@
+from encrypt import*
+from reedsave import *
+
+
 ALPHABET = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+key = get_text('task1/key.txt')
+text = get_text('task1/text.txt')
+encrypted = keyword_cipher(text, key)
