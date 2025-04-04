7bf30278e417978755cb62e73e5d67bf0c31b065
limekh <167549380+limekh@users.noreply.github.com>
2025-04-04 14:22:14 +0400

    Update tak1/consts.py

--- lab_1/task1/consts.py
+++ lab_1/task1/consts.py
@@ -5,5 +5,5 @@ from reedsave import *
 ALPHABET = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 KEY = get_text('task1/key.txt')
 TEXT = get_text('task1/text.txt')
-ENCRYPTED = keyword_cipher(text, key)
+ENCRYPTED = keyword_cipher(TEXT, KEY)
 ENCRYPTED_TEXT = "task1/cipher_text.txt"
