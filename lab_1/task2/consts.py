efb78dff526464285c6e304dcf38ba5dfe1c9f77
limekh <167549380+limekh@users.noreply.github.com>
2025-04-04 14:17:43 +0400

    Update task2/consts.py

--- lab_1/task2/consts.py
+++ lab_1/task2/consts.py
@@ -2,11 +2,11 @@ from reedsave import *
 from task2.decrypt import *
 
 
-auto_key = 'task2/auto_key.json'
-encrypt_text = get_text('task2/cod13.txt')
-decrypted = decrypt(encrypt_text, read_json('task2/auto_key.json'))
-decrypted_text = 'task2/decrypted_text.txt'
-freq = 'task2/freq.json'
-freq_ru = 'task2/frequencies.json'
-real_decrypt = decrypt(encrypt_text, read_json('task2/real_key.json'))
-real_decrypted_text = 'task2/real_decrypt.txt'
+AUTO_KEY = 'task2/auto_key.json'
+ENCRYPTED_TEXT = get_text('task2/cod13.txt')
+DECRYPTED = decrypt(encrypt_text, read_json('task2/auto_key.json'))
+DECRYPTED_TEXT = 'task2/decrypted_text.txt'
+FREQ = 'task2/freq.json'
+FREQ_RU = 'task2/frequencies.json'
+REAL_DECRYPT = decrypt(encrypt_text, read_json('task2/real_key.json'))
+REAL_DECRYPTED_TEXT = 'task2/real_decrypt.txt'
