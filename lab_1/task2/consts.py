bfcadcafb7f1e19e01c76c2eb60aa2b16bd36331
limekh <167549380+limekh@users.noreply.github.com>
2025-04-04 14:24:52 +0400

    Update task2/consts.py

--- lab_1/task2/consts.py
+++ lab_1/task2/consts.py
@@ -4,9 +4,9 @@ from task2.decrypt import *
 
 AUTO_KEY = 'task2/auto_key.json'
 ENCRYPTED_TEXT = get_text('task2/cod13.txt')
-DECRYPTED = decrypt(encrypt_text, read_json('task2/auto_key.json'))
+DECRYPTED = decrypt(ENCRYPTED_TEXT, read_json('task2/auto_key.json'))
 DECRYPTED_TEXT = 'task2/decrypted_text.txt'
 FREQ = 'task2/freq.json'
 FREQ_RU = 'task2/frequencies.json'
-REAL_DECRYPT = decrypt(encrypt_text, read_json('task2/real_key.json'))
+REAL_DECRYPT = decrypt(ENCRYPTED_TEXT, read_json('task2/real_key.json'))
 REAL_DECRYPTED_TEXT = 'task2/real_decrypt.txt'
