ee0281cfc2efd83c9dd32406f892ad7dbbd57efa
limekh <167549380+limekh@users.noreply.github.com>
2025-04-04 13:08:41 +0400

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -5,10 +5,10 @@ from task2.decrypt import *
 
 def main():
     # task1
-    key = get_text('key.txt')
-    text = get_text('text.txt')
+    key = get_text('task1/key.txt')
+    text = get_text('task1/text.txt')
     encrypted = keyword_cipher(text, key)
-    save_text("cipher_text.txt", encrypted)
+    save_text("task1/cipher_text.txt", encrypted)
 
     # task 2
     encrypt_text = get_text('task2/cod13.txt')
@@ -16,6 +16,8 @@ def main():
     save_json('task2/auto_key.json', create_key(encrypt_text, read_json('task2/frequencies.json')))
     decrypt_text = decrypt(encrypt_text, read_json('task2/auto_key.json'))
     save_text('task2/decrypted_text.txt', decrypt_text)
+    real_decrypt = decrypt(encrypt_text, read_json('task2/real_key.json'))
+    save_text('task2/real_decrypt.txt', real_decrypt)
 
 
 if __name__ == "__main__":
