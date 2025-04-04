abefa4627dc14117e0a58738dbb8dd045b51469f
limekh <167549380+limekh@users.noreply.github.com>
2025-04-04 13:32:07 +0400

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,22 +1,15 @@
-from reedsave import *
-from task1.encrypt import *
-from task2.decrypt import *
+from task1.consts import *
+from task2.consts import *
 
 
 def main():
     # task1
-    key = get_text('task1/key.txt')
-    text = get_text('task1/text.txt')
-    encrypted = keyword_cipher(text, key)
     save_text("task1/cipher_text.txt", encrypted)
 
     # task 2
-    encrypt_text = get_text('task2/cod13.txt')
     save_json('task2/freq.json', freq_analysis(encrypt_text))
     save_json('task2/auto_key.json', create_key(encrypt_text, read_json('task2/frequencies.json')))
-    decrypt_text = decrypt(encrypt_text, read_json('task2/auto_key.json'))
     save_text('task2/decrypted_text.txt', decrypt_text)
-    real_decrypt = decrypt(encrypt_text, read_json('task2/real_key.json'))
     save_text('task2/real_decrypt.txt', real_decrypt)
 
 
