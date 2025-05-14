cb9601e99a698a803aae92bd9bc2048aeb71838b
Milekhin <sdgright@yandex.ru>
2025-05-14 15:15:33 +0400

    add main.py, results files and change consts.py

---
+++ lab_2/main.py
@@ -0,0 +1,32 @@
+from consts import *
+from reedsave import *
+from tests import *
+
+
+def all_tests(seq: str, name: str, out: str):
+    """
+
+    :param seq:
+    :param name:
+    :param out:
+    :return:
+    """
+    result = {
+        f"{name} sequence": seq,
+        f"Frequency bit test (P-value)": freq_bit_test(seq),
+        f"Test for identical consecutive bits (P-value)": test_for_identical_bits(seq),
+        f"Test for the longest sequence of ones in a block (P-value)": longest_sequence_test(seq)
+    }
+    save_json(out, result)
+
+
+def main():
+    cpp_seq = get_text(cpp_seq_txt)
+    java_seq = get_text(java_seq_txt)
+
+    all_tests(cpp_seq, "C++", test_results_cpp)
+    all_tests(java_seq, "Java", test_results_java)
+
+
+if __name__ == "__main__":
+    main()
