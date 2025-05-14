89ecbecd61305f1a9e5ac04eaa99bbb1e50c3b4b
Milekhin <sdgright@yandex.ru>
2025-05-14 13:52:53 +0400

    add reedsave.py for json

---
+++ lab_2/reedsave.py
@@ -0,0 +1,49 @@
+import json
+
+
+def read_json(dir: str) -> dict:
+    """
+    Reads JSON file
+    :param dir: Path to the JSON file
+    :return: JSON file
+    """
+    try:
+        with open(dir, mode='r', encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError as e:
+        print(f"File does not exist: {e}")
+    except PermissionError as e:
+        print(f"Can't access this file: {e}")
+    except Exception as e:
+        print(f"Something wrong:{e}")
+
+
+def get_text(dir: str) -> str:
+    """
+    Opens text file
+    :param dir: path to the .txt file
+    :return: .txt file as a string
+    """
+    try:
+        with open(dir, mode='r', encoding="utf-8") as file:
+            return file.read()
+    except FileNotFoundError as e:
+        print(f"File does not exist: {e}")
+
+
+def save_json(dir: str, data: dict) -> None:
+    """
+    Saves dictionary as json file
+    :param dir: path to save .json file
+    :param data: dictionary to save
+    :return: None
+    """
+    try:
+        with open(dir, mode='w', encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False)
+    except FileNotFoundError as e:
+        print(f"File does not exist: {e}")
+    except PermissionError as e:
+        print(f"Can't access this file: {e}")
+    except Exception as e:
+        print(f"Something went wrong: {e}")
