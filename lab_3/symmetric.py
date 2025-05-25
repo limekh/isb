8f2b4237f96e4c6b39677f71ea550687f82de0d2
Milekhin <sdgright@yandex.ru>
2025-05-25 19:57:49 +0400

    add symmetric.py

---
+++ lab_3/symmetric.py
@@ -0,0 +1,60 @@
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import os
+
+from fileManager import FilesManager
+
+
+class Asymmetric:
+
+    @staticmethod
+    def generate_key(key_len):
+        if key_len not in [64, 128, 192]:
+            raise ValueError("Wrong key length! Must be 64, 128 or 192 bits!")
+        key = os.urandom(key_len // 8)
+        return key
+
+    @staticmethod
+    def encrypt_text(key, text_name, path_to_save):
+        plaintext = FilesManager.get_bytes(text_name)
+        iv = os.urandom(8)  # 3DES использует 8-байтовый IV
+
+        # Применяем padding к данным
+        padder = padding.PKCS7(algorithms.TripleDES.block_size).padder()
+        padded_text = padder.update(plaintext) + padder.finalize()
+
+        # Шифруем
+        cipher = Cipher(
+            algorithms.TripleDES(key),
+            modes.CBC(iv),
+            backend=default_backend()
+        )
+        encryptor = cipher.encryptor()
+        ciphertext = encryptor.update(padded_text) + encryptor.finalize()
+        ciphertext = iv + ciphertext
+
+        FilesManager.write_bytes(path_to_save, ciphertext)
+
+    @staticmethod
+    def decrypt_text(key, ciphertext_path, path_to_save):
+        cipher_text = FilesManager.get_bytes(ciphertext_path)
+        iv = cipher_text[:8]
+
+        cipher = Cipher(algorithms.TripleDES(key), modes.CBC(iv))
+        decryptor = cipher.decryptor()
+        decrypted_text = decryptor.update(cipher_text) + decryptor.finalize()
+
+        unpadder = padding.PKCS7(algorithms.TripleDES.block_size).unpadder()
+        depadder_dc_text = unpadder.update(decrypted_text) + unpadder.finalize()
+
+        FilesManager.write_txt(path_to_save, depadder_dc_text.decode('UTF-8'))
+        return depadder_dc_text.decode('UTF-8')
+
+    @staticmethod
+    def serialization_symmetric_key(path_to_save, key):
+        FilesManager.write_bytes(path_to_save, key)
+
+    @staticmethod
+    def deserialization_symmetric_key(file_name):
+        return FilesManager.get_bytes(file_name)
