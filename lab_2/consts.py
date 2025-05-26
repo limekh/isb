34c3c386a730aff7cc4ff23df6c7f3648cfbc69e
limekh <167549380+limekh@users.noreply.github.com>
2025-05-26 11:02:34 +0400

    Delete lab_2 directory

--- lab_2/consts.py
+++
@@ -1,8 +0,0 @@
-cpp_seq_txt = "cppSeq.txt"
-java_seq_txt = "javaSeq.txt"
-
-test_results_cpp = "test_results_cpp.json"
-test_results_java = "test_results_java.json"
-
-PI_I = [0.2148, 0.3672, 0.2305, 0.1875]
-
